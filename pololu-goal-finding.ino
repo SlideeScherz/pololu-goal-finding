214afe9fa2b5e369e34418c0e6743bb50aee1f43
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-04-07 21:40:17 -0400

    Add project files.

---
+++ pololu-goal-finding.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		pololu_goal_finding.ino
+ Created:	4/7/2022 9:39:32 PM
+ Author:	Scott
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
