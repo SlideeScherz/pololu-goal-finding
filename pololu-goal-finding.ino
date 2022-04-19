5619d8c34a91295f5820fb3f57a2872ea7bfe96d
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-04-19 13:21:28 -0400

    PID and motor optimizations

--- pololu-goal-finding.ino
+++ pololu-goal-finding.ino
@@ -2,6 +2,10 @@
  Name: polulo_goal_finding.ino
  Created:	4/15/2022 12:59:32 PM
  Authors:	Thomas Diaz-Piedra, Scott Scherzer, Christopher Fioti
+ Version: 1.0.1
+
+ All distance and measurements are expressed in cm
+ All trig calculations are expressed in radians
 */
 
 #include <Pololu3piPlus32U4.h>
@@ -12,28 +16,18 @@ Encoders encoders;
 Motors motors;
 Buzzer buzzer;
 
-/* encoder data */
+/**
+ * encoder data
+ * init all encoder data to 0 since no distance has been traveled
+ */
 
 // timers 
 unsigned long encodersT1, encodersT2;
-const unsigned long ENCODER_PERIOD = 20;
+const unsigned long ENCODER_PERIOD = 20UL;
 
 // encoder counts
 long countsLeft = 0, countsRight = 0;
 long prevLeft = 0, prevRight = 0;
-
-//TODO: make calculation a const
-const float CLICKS_PER_ROTATION = 12.0f;
-const float GEAR_RATIO = 75.81f;
-const float WHEEL_DIAMETER = 3.2f; //TODO unused
-const float WHEEL_CIRCUMFRENCE = 10.0531f;
-
-/* location data */
-
-const float B = 8.5f;
-
-// distance before applying dampening to slow down 
-const float DAMPEN_RANGE = 20.0f;
 
 // distance traveled by wheel in cm
 float sL = 0.0f, sR = 0.0f;
@@ -44,8 +38,30 @@ float prevSL = 0.0f, prevSR = 0.0f;
 // difference between current and previous distance traveled
 float sL_Delta = 0.0f, sR_Delta = 0.0f;
 
-// position coordinates
-float x = 0.0f, y = 0.0f, theta = 0.0f;
+// TODO: make calculation a const
+// wheel and encoder constants
+const float CLICKS_PER_ROTATION = 12.0f;
+const float GEAR_RATIO = 75.81f;
+const float WHEEL_DIAMETER = 3.2f; 
+const float WHEEL_CIRCUMFRENCE = WHEEL_DIAMETER * PI; // TODO remover circumf and make one formula
+
+// distance between the 2 drive wheels from the center point of the contact patches
+const float B = 8.5f;
+//TEST const float B = 8.255f;
+
+/** 
+ * position data 
+ * init position objects to start at (0,0)
+ */
+
+// distance before applying dampening break force 
+const float DAMPEN_RANGE = 20.0f;
+
+// position cartesian coordinates
+float x = 0.0f, y = 0.0f; 
+
+// angle robot is facing in radians
+float theta = 0.0f;
 
 // change in position between last 2 intervals
 float xDelta = 0.0f, yDelta = 0.0f, thetaDelta = 0.0f;
@@ -58,7 +74,7 @@ float positionDelta = 0.0f;
 // index of GOAL array to select which goal to navigate to 
 int currentGoal = 0;
 
-//len of GOALS array, how many goals we want to navigate to 
+// len of GOALS array, how many goals we want to navigate to 
 const int NUMBER_OF_GOALS = 4;
 
 // goal containers
@@ -78,32 +94,50 @@ float currentGoalDistance = startGoalDistance;
 
 /* motor data */
 
-// speed constants
-const float MOTOR_BASE_SPEED = 75.0f, MOTOR_MIN_SPEED = 40.0f, MOTOR_MAX_SPEED = 150.0f;
+// TODO throttle
+/**
+ * explore -40 to allow sharp turns based on magnitude of error
+ * If very close to goal, turn sharper like a tank turn, setSpeeds(-40, 40)
+ * -1% would be -MIN, -100% would be -MAX
+ * 0% would be off (idle) 0
+ * 1% would be +MIN, 100% would be +MAX
+ * Base speed would be +50%, can be offset by a large error
+ * A larger error will reduce linear speed and increase rotational speed
+ */
 
-// init speed to base
-float leftSpeed = 0.0f, rightSpeed = 0.0f;
+// speed limits
+const float MOTOR_MIN_SPEED = 40.0f, MOTOR_MAX_SPEED = 150.0f;
+
+// speed constants
+const float MOTOR_BASE_SPEED = 75.0f, MOTOR_IDLE = 0.0f;
+
+// wheelSpeed containers. Set by PID output
+float leftSpeed = MOTOR_IDLE, rightSpeed = MOTOR_IDLE;
 
 // timers
 unsigned long motorT1, motorT2;
-const unsigned long MOTOR_PERIOD = 20;
+const unsigned long MOTOR_PERIOD = 20UL;
 
 /* PID data */
 
+// TODO use rounding to smooth PID corrections and errors
+
+// TUNE KP lower
 // proportional error factor
 const float KP = 100.0f;
 
 // suggested PID correction object
 float PIDCorrection = 0.0f;
 
-// error data
+// current theta vs theta of goal. Derived from arctan
 float currentError = 0.0f;
 
 /* debugging switches */
+
 bool bEncoderDebug = false;
-bool bPositionDebug = false;
+bool bPositionDebug = true;
 bool bMotorDebug = false;
-bool bPIDDebug = false;
+bool bPIDDebug = true;
 
 void setup()
 {
@@ -115,13 +149,18 @@ void loop()
 {
   if (currentGoal < NUMBER_OF_GOALS)
   {
-    checkEncoders();
-    setMotors(PIDCorrection);
+    readEncoders();
+    setMotors();
     checkGoalStatus();
   }
 }
 
-void checkEncoders()
+/**
+* Read encoder data to calculate the distance traveled
+* Also, calculate the change in position to calculate position
+* @returns void sets sL and sR delta for position calculation
+*/
+void readEncoders()
 {
   encodersT1 = millis();
 
@@ -131,10 +170,6 @@ void checkEncoders()
     // read current encoder count
     countsLeft += encoders.getCountsAndResetLeft();
     countsRight += encoders.getCountsAndResetRight();
-
-    // write previous distance traveled
-    prevSL = sL;
-    prevSR = sR;
 
     // update the distance traveled by each wheel
     sL += ((countsLeft - prevLeft) / (CLICKS_PER_ROTATION * GEAR_RATIO) * WHEEL_CIRCUMFRENCE);
@@ -148,6 +183,10 @@ void checkEncoders()
     prevLeft = countsLeft;
     prevRight = countsRight;
 
+    // write previous distance traveled
+    prevSL = sL;
+    prevSR = sR;
+
     // reset timer
     encodersT2 = encodersT1;
 
@@ -156,24 +195,31 @@ void checkEncoders()
   }
 }
 
+/**
+ * Update the robots current position using wheel encoder data
+ * @param none reads change from sL and sR
+ * @returns void updates x,y, theta and currentGoalDistance
+ */
 void getPosition()
 {
-  // update distances vs their delta
+  // update position using the deltas of each
   positionDelta = (sL_Delta + sR_Delta) / 2.0f;
   thetaDelta = (sR_Delta - sL_Delta) / B;
 
+  // get polar coordinates of x and y
   xDelta = positionDelta * cos(theta + thetaDelta / 2.0f);
   yDelta = positionDelta * sin(theta + thetaDelta / 2.0f);
 
-  //update the current location
+  // update coordinates
   x += xDelta;
   y += yDelta;
   theta += thetaDelta;
+
+  // calculate linear distance to goal using updated position
   currentGoalDistance = sqrt(sq(xGoal - x) + sq(y - yGoal));
 
   if (bPositionDebug) debugPosition();
 
-  //TODO New. Test
   // send position data to PID controller to get a correction
   getPIDCorrection();
 }
@@ -182,15 +228,23 @@ void getPosition()
  * get a proportionate correction based on current theta vs goal
  * a positive currentError will suggest a left turn
  * a negative currentError will suggest a right turn
+ * @param none. Reads position data and goal to set theta error
  * @returns void set PIDcorrection to a proportional angle correction
  */
 void getPIDCorrection()
 {
   currentError = theta - atan2(yGoal - y, xGoal - x);
 
-  if (bPIDDebug) debugPID();
+  /*
+  if (currentError >= 0.10f || currentError >= -0.10f)
+  {
+    currentError = 0.0f;
+  }
+  */
 
   PIDCorrection = KP * currentError;
+
+  if (bPIDDebug) debugPID();
 }
 
 /**
@@ -232,18 +286,19 @@ void checkGoalStatus()
 
 /**
  * set motor speeds with PID input
- * @param controllerOutput PID result of angle correction
+ * adjusts wheel speeds individually to controll angle based on error
+ * @param none reads PID result of angle correction
  * @returns void. sets left and right global wheelspeeds.
  */
-void setMotors(float controllerOutput)
+void setMotors()
 {
   motorT1 = millis();
 
   if (motorT1 > motorT2 + MOTOR_PERIOD)
   {
 
-    leftSpeed = angleController(controllerOutput, 1);
-    rightSpeed = angleController(controllerOutput, -1);
+    leftSpeed = angleController(PIDCorrection, 1);
+    rightSpeed = angleController(PIDCorrection, -1);
 
     motors.setSpeeds(leftSpeed, rightSpeed);
 
@@ -268,7 +323,7 @@ float angleController(float thetaCorrection, int polarity)
   if (currentGoalDistance <= DAMPEN_RANGE)
     wheelSpeed *= (currentGoalDistance / DAMPEN_RANGE);
 
-  //check max and min speed limits
+  // check max and min speed limits
   if (wheelSpeed < MOTOR_MIN_SPEED) wheelSpeed = MOTOR_MIN_SPEED;
   else if (wheelSpeed > MOTOR_MAX_SPEED) wheelSpeed = MOTOR_MAX_SPEED;
 
@@ -278,7 +333,7 @@ float angleController(float thetaCorrection, int polarity)
 void debugPID()
 {
   Serial.print("PID ");
-  Serial.print("currentError: ");
+  Serial.print("err: ");
   Serial.print(currentError);
   Serial.print(" output: ");
   Serial.println(PIDCorrection);
@@ -309,13 +364,20 @@ void debugMotors()
 
 void debugPosition()
 {
-  Serial.print("POS ");
-  Serial.print("goal: ");
+  Serial.print("POS (");
+  Serial.print(x);
+  Serial.print(", ");
+  Serial.print(y);
+  Serial.print(", ");
+  Serial.print(theta);
+  Serial.print(")");
+  Serial.print(" tgt (");
   Serial.print(xGoal);
   Serial.print(", ");
   Serial.print(yGoal);
+  Serial.print(")");
   Serial.print(" goalDist: ");
-  Serial.print(currentGoalDistance);
+  Serial.println(currentGoalDistance);
 
   /* uncomment if needed
   Serial.print(" deltas: (");
@@ -326,12 +388,4 @@ void debugPosition()
   Serial.print(thetaChange);
   Serial.print(")");
   */
-
-  Serial.print(" pos: (");
-  Serial.print(x);
-  Serial.print(", ");
-  Serial.print(y);
-  Serial.print(", ");
-  Serial.print(theta);
-  Serial.println(")");
 }
